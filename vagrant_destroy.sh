20ce9b917a51b779704309c1680bb4f1741eab2d
Sergio Romera <sergio.romera@enterprisedb.com>
2023-08-24 17:50:01 +0200

    Version 1.0

---
+++ vagrant_destroy.sh
@@ -0,0 +1,7 @@
+vagrant destroy -f node0 &
+vagrant destroy -f node1 &
+vagrant destroy -f node2 &
+vagrant destroy -f node3 &
+vagrant destroy -f node4 &
+vagrant destroy -f node5 &
+vagrant destroy -f node6 &
---
+++ vagrant_up.sh
@@ -0,0 +1,7 @@
+vagrant up node0
+vagrant up node1 &
+vagrant up node2 &
+vagrant up node3 &
+vagrant up node4 &
+vagrant up node5 &
+vagrant up node6 &
